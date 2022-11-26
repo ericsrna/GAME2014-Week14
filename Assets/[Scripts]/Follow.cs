fd6abf3d4cebe7ff3e9c6a7984cfd105ca5ff57a
Eric Na <eric.srna@voilalearning.com>
2022-11-26 16:09:54 -0500

    added follow script

---
+++ Assets/[Scripts]/Follow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Target
+{
+    public Transform transform;
+    public Vector2 offset;
+    public bool x;
+    public bool y;
+}
+
+public class Follow : MonoBehaviour
+{
+    public Target target;
+
+
+    void Update()
+    {
+        transform.position = new Vector2(
+            (target.x) ? target.transform.position.x + target.offset.x : transform.position.x,
+            (target.y) ? target.transform.position.y + target.offset.y : transform.position.y
+        );
+    }
+}
